dd1a7d7a83f6b5a97367100e6c6f85ed67442be0
Wen YE <whusnoopy@gmail.com>
2023-06-16 13:06:42 +0800

    MOD: gui fetch works

--- gui.py
+++ gui.py
@@ -22,112 +22,128 @@ OUTPUT_ROWS = 40
 logging.config.dictConfig(config.LOGGING_CONF)
 logger = logging.getLogger(__name__)
 
+buffer = []
+
 
 class GUILoggingHandler(logging.StreamHandler):
-    def __init__(self):
+    def __init__(self, window):
         logging.StreamHandler.__init__(self)
+        self._window = window
 
     def emit(self, record):
         global buffer  # pylint: disable=W0603
         format_msg = self.format(record)
-        msg = f'{record.asctime} [{record.levelname}] {format_msg}'
+        msg = f"{record.asctime} [{record.levelname}] {format_msg}"
         if buffer:
-            buffer = buffer[1-OUTPUT_ROWS:]
+            buffer = buffer[1 - OUTPUT_ROWS :]
             buffer.append(msg)
         else:
             buffer = [msg]
         try:
-            window['-LOUT-'].update(value='\n'.join(buffer))
+            self._window["-LOUT-"].update(value="\n".join(buffer))
         except NameError:
             # GUI window not inited, just pass
             pass
-
-
-buffer = []
-ch = GUILoggingHandler()
-ch.setLevel(logging.INFO)
-
-logging.getLogger("").addHandler(ch)
 
 
 def run_server():
     app.run()
 
 
-cookie = Crawler.load_cookie()
-fetched_info = {}
-if cookie:
-    cookie_str = cookie.values()[0]
-    cookie_json = json.loads(urllib.parse.unquote(cookie_str))
-    fetched_info = update_fetch_info(cookie_json['userId'])
+def run_fetch(uid, fetch_status, fetch_gossip, fetch_album, fetch_blog):
+    fetched = fetch_user(
+        uid,
+        fetch_status=fetch_status,
+        fetch_gossip=fetch_gossip,
+        fetch_album=fetch_album,
+        fetch_blog=fetch_blog
+    )
+
+    if not fetched:
+        logger.info("nothing need to fetch, just test login")
+
+    if fetched:
+        update_fetch_info(uid)
 
 
-form_column = [
-    [sg.Text("人人网账号"), sg.Input("", size=(24, 1), key="-INPUT-EMAIL-")],
-    [sg.Text("人人网密码"), sg.Input("", size=(24, 1), key="-INPUT-PASSWORD-", password_char="*")],
-    [
-        sg.Checkbox("状态", key="-FETCH-STATUS-"),
-        sg.Checkbox("日志", key="-FETCH-BLOG-"),
-        sg.Checkbox("相册", key="-FETCH-ALBUM-"),
-        sg.Checkbox("留言", key="-FETCH-GOSSIP-")
-    ],
-    [sg.Button("开始获取", key="-FETCH-")],
-    [sg.Button("开启服务", key="-START-")]
-]
+def main():
 
-log_column = [
-    [sg.Text(fetched_info.get('name', 'unknown'), size=(OUTPUT_COLUMNS, OUTPUT_ROWS), key="-LOUT-")]
-]
+    cookie = Crawler.load_cookie()
+    fetched_info = {}
+    if cookie:
+        cookie_str = cookie.values()[0]
+        cookie_json = json.loads(urllib.parse.unquote(cookie_str))
+        fetched_info = update_fetch_info(cookie_json["userId"])
 
-layout = [
-    [
-        sg.Column(form_column),
-        sg.VSeparator(),
-        sg.Column(log_column)
+    form_column = [
+        [sg.Text("人人网账号"), sg.Input("", size=(24, 1), key="-INPUT-EMAIL-")],
+        [
+            sg.Text("人人网密码"),
+            sg.Input("", size=(24, 1), key="-INPUT-PASSWORD-", password_char="*"),
+        ],
+        [
+            sg.Checkbox("状态", key="-FETCH-STATUS-"),
+            sg.Checkbox("日志", key="-FETCH-BLOG-"),
+            sg.Checkbox("相册", key="-FETCH-ALBUM-"),
+            sg.Checkbox("留言", key="-FETCH-GOSSIP-"),
+        ],
+        [sg.Button("开始获取", key="-FETCH-")],
+        [sg.Button("开启服务", key="-START-")],
     ]
-]
 
-window = sg.Window("人人网备份小工具", layout, margins=(20, 20))
-svr = None
+    log_column = [
+        [
+            sg.Text(
+                fetched_info.get("name", "unknown"),
+                size=(OUTPUT_COLUMNS, OUTPUT_ROWS),
+                key="-LOUT-",
+            )
+        ]
+    ]
 
-while True:
-    event, values = window.read()
+    layout = [[sg.Column(form_column), sg.VSeparator(), sg.Column(log_column)]]
 
-    if event == "OK" or event == sg.WIN_CLOSED:
-        if svr:
-            logger.info('terminate web server')
-        break
+    window = sg.Window("人人网备份小工具", layout, margins=(20, 20))
+    svr = None
+    ch = GUILoggingHandler(window)
+    ch.setLevel(logging.INFO)
+    logging.getLogger("").addHandler(ch)
 
-    if event == "-FETCH-":
-        email = values['-INPUT-EMAIL-']
-        password = values['-INPUT-PASSWORD-']
+    while True:
+        event, values = window.read()
 
-        fetch_status = values['-FETCH-STATUS-']
-        fetch_blog = values['-FETCH-BLOG-']
-        fetch_album = values['-FETCH-ALBUM-']
-        fetch_gossip = values['-FETCH-GOSSIP-']
+        if event == "OK" or event == sg.WIN_CLOSED:
+            if svr:
+                logger.info("terminate web server")
+            break
 
-        prepare_db()
+        if event == "-FETCH-":
+            email = values["-INPUT-EMAIL-"]
+            password = values["-INPUT-PASSWORD-"]
 
-        config.crawler = Crawler(email, password, cookie)
-        uid = config.crawler.uid
+            fetch_status = values["-FETCH-STATUS-"]
+            fetch_blog = values["-FETCH-BLOG-"]
+            fetch_album = values["-FETCH-ALBUM-"]
+            fetch_gossip = values["-FETCH-GOSSIP-"]
 
-        fetched = fetch_user(
-            uid,
-            fetch_status=fetch_status,
-            fetch_gossip=fetch_gossip,
-            fetch_album=fetch_album,
-            fetch_blog=fetch_blog
-        )
+            prepare_db()
 
-        if not fetched:
-            logger.info("nothing need to fetch, just test login")
+            config.crawler = Crawler(email, password, cookie)
+            uid = config.crawler.uid
 
-        if fetched:
-            update_fetch_info(uid)
+            fetch_thread = threading.Thread(
+                target=run_fetch,
+                args=(uid, fetch_status, fetch_gossip, fetch_album, fetch_blog),
+                daemon=True,
+            )
+            fetch_thread.start()
 
-    elif event == '-START-':
-        svr = threading.Thread(target=run_server, args=(), daemon=True)
-        svr.start()
+        elif event == "-START-":
+            svr = threading.Thread(target=run_server, args=(), daemon=True)
+            svr.start()
 
-window.close()
+    window.close()
+
+
+if __name__ == "__main__":
+    main()
